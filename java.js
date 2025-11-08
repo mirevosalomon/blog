bee4e1734c328aa4ce709f8e039657619e32dff7
mirevosalomon <salomonmirevo@gmail.com>
2025-11-08 16:20:29 +0100

    premier comit

---
+++ java.js
@@ -0,0 +1,11 @@
+// Script principal pour le blog de MIREVO MULEMBERI SALOMON
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Bienvenue sur le blog de MIREVO MULEMBERI SALOMON !");
+  
+  // Exemple de fonctionnalité simple :
+  const titre = document.querySelector("h1");
+  titre.addEventListener("click", () => {
+    alert("Merci de visiter mon blog !");
+  });
+});
